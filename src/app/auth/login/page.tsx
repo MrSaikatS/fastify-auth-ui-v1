d1281e1b93f2c818507f24420d1ee69a85a3b5da
Rishav Mondal <mondalrishav25@gmail.com>
2025-03-25 22:05:10 +0530

    login & register route added

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <>
+      <div className="grid h-dvh place-items-center">
+        <div className="">Login page</div>
+      </div>
+    </>
+  );
+};
+
+export default page;
---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <>
+      <div className="grid h-dvh place-items-center">
+        <div className="">Register page</div>
+      </div>
+    </>
+  );
+};
+
+export default page;
